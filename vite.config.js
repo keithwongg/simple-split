9df00cba58ec516ee934e917854cf304a9d44060
Keith Wong <keithwjh@outlook.sg>
2025-03-01 17:54:41 +0800

    Config for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import Components from 'unplugin-vue-components/vite'
-import {PrimeVueResolver} from '@primevue/auto-import-resolver'
+import { PrimeVueResolver } from '@primevue/auto-import-resolver'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/simple-split/",
   plugins: [
     vue(),
     vueDevTools(),
